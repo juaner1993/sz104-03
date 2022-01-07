c4f9d4176d2d069312aaa48451c70818125a392b
juaner1993 <1003591756@qq.com>
2022-01-07 14:30:35 +0800

    init

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * 包名:com.itheima.pojo
+ *
+ * @author juaner
+ * 日期:2022-01-07  14:20
+ */
+
+public class User {
+    private String userName;
+    private Integer age;
+}
